6bcd46ed11e64ccfd60b2fce408b1b05a3427c07
Eragon580101 <erewhonaden23@gmail.com>
2023-04-16 19:55:31 +0545

    Age Calculator

--- src/Component/Input/Input.jsx
+++ src/Component/Input/Input.jsx
@@ -6,8 +6,8 @@ const Input = forwardRef((props, ref) => {
 
     return (
         <div className="input">
-            <label ref={ref[2]} aria-label='label' htmlFor="input" className='input__label'>{props.label}</label>
-            <input ref={ref[0]} type='number' name='input' id='input' className='input__field' placeholder={props.placeholder}/>
+            <label ref={ref[2]} htmlFor="input" className='input__label'>{props.label}</label>
+            <input ref={ref[0]} type='number' name='input' className='input__field' placeholder={props.placeholder}/>
             <p ref={ref[1]} className='error hidden'>This field is required</p>
         </div>
     )
